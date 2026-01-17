6b5ddb9cf7c76ed69b3705b72f90e7685cc219ff
Michele Coppola <michele.coppola17@outlook.it>
2026-01-17 18:52:47 +0100

    Finish encapsulation section

---
+++ python/learn_oop_in_python/03_encapsulation/01_encapsulation/main.py
@@ -0,0 +1,20 @@
+"""
+Assignment:
+Complete the Wizard class's constructor.
+
+1. Set 2 private properties (be sure to include the private __ prefix):
+    1. stamina
+    2. intelligence
+2. Set 3 public properties:
+    1. name: Use the value passed into the constructor
+    2. health: 100x the value of "stamina"
+    3. mana: 10x the value of "intelligence"
+"""
+
+class Wizard:
+    def __init__(self, name, stamina, intelligence):
+        self.name = name
+        self.__stamina = stamina
+        self.__intelligence = intelligence
+        self.health = self.__stamina * 100
+        self.mana = self.__intelligence * 10
---
+++ python/learn_oop_in_python/03_encapsulation/04_wizard_duel/main.py
@@ -0,0 +1,31 @@
+"""
+Assignment:
+Complete the following methods on the Wizard class:
+
+1. get_fireballed() should:
+    - Reduce the fireball_damage by the wizard's __stamina
+    - Reduce the wizard's health by the resulting fireball_damage
+2. drink_mana_potion() should:
+    - Increase the potion_mana by the wizard's __intelligence
+    - Increase the wizard's mana by the resulting potion_mana
+
+Both methods operate directly on the instance of the class (self). They take one input and return no values explicitly.
+"""
+
+class Wizard:
+    def __init__(self, name, stamina, intelligence):
+        self.name = name
+        self.__stamina = stamina
+        self.__intelligence = intelligence
+        self.mana = self.__intelligence * 10
+        self.health = self.__stamina * 100
+
+    # don't touch above this line
+
+    def get_fireballed(self, fireball_damage):
+        fireball_damage -= self.__stamina
+        self.health -= fireball_damage
+
+    def drink_mana_potion(self, potion_mana):
+        potion_mana += self.__intelligence
+        self.mana += potion_mana

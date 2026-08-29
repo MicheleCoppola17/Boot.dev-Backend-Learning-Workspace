8a5c69e2546f2161beb7ed9ff935709d6484d198
Michele Coppola <michele.coppola17@outlook.it>
2026-08-29 15:09:59 +0200

    complete enums section

---
+++ c/01_learn_memory_management/04_enums/04_sizeof_enum/size.c
@@ -0,0 +1,31 @@
+/*
+Assignment
+At the start of main(), print the size of the two enums already defined for you, in the format:
+
+The size of BigNumbers is Y bytes
+The size of HttpErrorCode is X bytes
+
+Remember that %zu is the format specifier for size_t.
+*/
+
+#include <stdio.h>
+
+typedef enum {
+  BIG = 123412341234,
+  BIGGER,
+  BIGGEST,
+} BigNumbers;
+
+typedef enum {
+  HTTP_BAD_REQUEST = 400,
+  HTTP_UNAUTHORIZED = 401,
+  HTTP_NOT_FOUND = 404,
+  HTTP_I_AM_A_TEAPOT = 418,
+  HTTP_INTERNAL_SERVER_ERROR = 500
+} HttpErrorCode;
+
+int main() {
+  printf("The size of BigNumbers is %zu bytes\n", sizeof(BigNumbers));
+  printf("The size of HttpErrorCode is %zu bytes\n", sizeof(HttpErrorCode));
+  return 0;
+}
